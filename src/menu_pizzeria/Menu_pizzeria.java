1a19fe3ceed992bdfd9d7e5363d45d30ce60819f
DAM123 <DAM123@IF03-21.iesmhp.local>
2020-11-30 16:05:46 +0100

    PRIMERA MODIFICACION MARIO

---
+++ src/menu_pizzeria/Menu_pizzeria.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package menu_pizzeria;
+
+/**
+ *
+ * @author DAM123
+ */
+public class Menu_pizzeria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Menu:");
+        System.out.println("    1- ");
+        
+    }
+    
+}
