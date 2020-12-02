d4867150f333030b2fc875b69c019c1003ef32c9
DAM123 <DAM123@IF03-21.iesmhp.local>
2020-12-02 17:36:17 +0100

    segunda MODIFICACION MARIO

--- src/menu_pizzeria/Menu_pizzeria.java
+++ src/menu_pizzeria/Menu_pizzeria.java
@@ -21,9 +21,9 @@ public class Menu_pizzeria {
       Scanner teclado=new Scanner(System.in);
       
         System.out.println("MENU:");
-        System.out.println("    1. Añadir pizzas");
-        System.out.println("    2. Añadir ingrediente");
-        System.out.println("    3. Cambiar tamño pizzas");
+        System.out.println("    1. Añadir pizzas basicas");
+        System.out.println("    2. Añadir pizzas con ingrediente");
+        System.out.println("    3. Cambiar tamaño de las pizzas");
         System.out.println("    4. Eliminar pizzas");
         System.out.println("    5. Calcular ticket");
         System.out.println("    6. Salir");
@@ -32,17 +32,21 @@ public class Menu_pizzeria {
         
         int contadorpizzasbasicas=0;
         int contadorpizzasingredientes=0;
+        int contadorPizzasBasicasGrandes=0;
+        int contadorPizzasIngredienteGrandes=0;
         
         while (opcion != 6) {
+            
             switch (opcion) {
+                
                 case 1:
-                    System.out.println("Cuantas pizzas desea pedir?");
+                    System.out.println("Cuantas pizzas basicas desea pedir?");
                     contadorpizzasbasicas = contadorpizzasbasicas + teclado.nextInt();
                     break;
-                    
+
                 case 2:
                     System.out.println("Cuantas pizzas con ingredientes deseas pedir?");
-                    contadorpizzasingredientes = contadorpizzasingredientes+teclado.nextInt();
+                    contadorpizzasingredientes = contadorpizzasingredientes + teclado.nextInt();
 
                     System.out.println("Que ingrediente desea añadir?"
                             + "(1.Queso, 2.tomate, 3.peperoni, 4.bacon)");
@@ -53,12 +57,30 @@ public class Menu_pizzeria {
                         respuesta = teclado.nextInt();
                     }
                     break;
-                
-                case 3:
-                    
-                    
-            }
 
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                default:
+                    System.out.println("No ha introducido una opcion correcta, vuelva a introducir una:");
+            }
+            System.out.println("MENU:");
+            System.out.println("    1. Añadir pizzas basicas");
+            System.out.println("    2. Añadir pizzas con ingrediente");
+            System.out.println("    3. Cambiar tamaño de las pizzas");
+            System.out.println("    4. Eliminar pizzas");
+            System.out.println("    5. Calcular ticket");
+            System.out.println("    6. Salir");
+            opcion = teclado.nextInt();
         }
     }
 
